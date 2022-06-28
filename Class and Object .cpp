024d7387dcfbc069356080d05351e57b4ba35cb2
Heeru Sojitra <107800573+HiruSojitra@users.noreply.github.com>
2022-06-28 12:07:54 +0530

    Create Class and Object .cpp

---
+++ Class and Object .cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class A
+{
+
+    public:
+
+      void show()
+      {
+          cout<<"hello world..."<<endl;
+      }
+};
+int main()
+{
+    A obj;
+
+    obj.show();
+
+    return 0;
+}
