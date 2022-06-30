c4930cb1ee4b6e868dcad87b280d55c6d04d439e
Heeru Sojitra <107800573+HiruSojitra@users.noreply.github.com>
2022-06-30 11:54:24 +0530

    Create 46 Access Specifiers.cpp
    i know how to use public,private and protected specifiers that's why i don't create program.....

---
+++ 46 Access Specifiers.cpp
@@ -0,0 +1,6 @@
+public:- members are accessible from outside the class.
+
+private:- members cannot be accessed or view from outside the class.
+
+protected:- members cannot be accessed from outside the class .
+            however, they can be accessed in inheritance classes.
