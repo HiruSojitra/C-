0e7cb01b0703f64ac99afdba09ab3aa308381c11
Heeru Sojitra <107800573+HiruSojitra@users.noreply.github.com>
2022-07-06 23:31:55 +0530

    Create 61 Introduction to Inheritance.cpp

---
+++ 61 Introduction to Inheritance.cpp
@@ -0,0 +1,11 @@
+        :- types of inheritance -:
+
+   1.single inheritance /simple inheritance
+
+   2.multiple inheritance
+
+   3.multilevel inheritance
+
+   4.hierarchical inheritance
+
+   5.hybrid inheritance    
